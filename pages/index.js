9e67cd75b5d4b27191c47c4694fa4330ea1cf57a
adeola <adeola.ak@outlook.com>
2024-02-17 23:19:47 -0500

    add site actions: add recipe, featured recipe, and dropdown

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,35 @@
+import { useState } from "react";
 import Header from "@/components/Header";
 
 export default function Home() {
-	return <Header />;
+	const [selected, setSelected] = useState("sort: newest");
+
+	const options = [
+		"newest",
+		"meal type (breakfast)",
+		"meal type (lunch)",
+		"meal type (dinner)",
+		"featured",
+	];
+
+	return (
+		<div>
+			<Header />
+			<div className="site-actions">
+				<p>Add a Recipe</p>
+				<p>Featured Recipes</p>
+
+				<select
+					value={selected}
+					onChange={(e) => setSelected(e.target.value)}
+				>
+					{options.map((option) => (
+						<option key={option} value={option}>
+							{option}
+						</option>
+					))}
+				</select>
+			</div>
+		</div>
+	);
 }
